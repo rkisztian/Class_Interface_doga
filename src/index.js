1ad79654403970e79713da341d219a67581dfa5a
Petrik Diak <diak@petrik.hu>
2022-11-10 15:19:03 +0100

    Sikeres adatfelvétel esetén üríti az űrlapot!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 "use strict";
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
+const Statue_1 = require("./Statue");
 let szobrok = [];
 (_a = document.getElementById('felvesz')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
     let szoborNev = document.getElementById('szobornev');
@@ -18,6 +19,7 @@ let szobrok = [];
     }
     else {
         keszitesiEvJo = false;
+        alert("Nem lehet a készitési év több, mint a jelenlegi évszám");
     }
     let kikialtasiAr = document.getElementById('ar');
     let kikialtasiArJo = false;
@@ -26,6 +28,22 @@ let szobrok = [];
     }
     else {
         keszitesiEvJo = false;
+        alert("Minimum 1Ft kell, hogy legyen");
     }
     let szoborMagassag = document.getElementById('magassag');
+    let szoborMagassagJo = false;
+    if (parseInt(szoborMagassag.value) >= 20) {
+        szoborMagassagJo = true;
+    }
+    else {
+        szoborMagassagJo = false;
+        alert("Minimum 20cm magasnak kell lennie a szobornak");
+    }
+    if (szoborNevVan && keszitesiEvJo && kikialtasiArJo && szoborMagassagJo) {
+        szobrok.push(new Statue_1.Statue(szoborNev.value, parseInt(keszitesiEv.value), parseInt(kikialtasiAr.value), parseInt(szoborMagassag.value)));
+        szoborNev.value = "";
+        keszitesiEv.value = "";
+        kikialtasiAr.value = "";
+        szoborMagassag.value = "";
+    }
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-
+import { Statue } from "./Statue";
 
 export interface Artwork{
     title : string;
@@ -11,37 +11,54 @@ let szobrok : Artwork[] = [];
 
 document.getElementById('felvesz')?.addEventListener('click', ()=>{
 
-    let szoborNev = document.getElementById('szobornev') as HTMLInputElement;
+    let szoborNev : HTMLInputElement = document.getElementById('szobornev') as HTMLInputElement;
     let szoborNevVan = false;
     if(szoborNev.value != ""){
         szoborNevVan = true;
     }else{
         szoborNevVan = false;
         
+        
     }
 
-
-    let keszitesiEv = document.getElementById('keszitesiEv') as HTMLInputElement;
+    
+    let keszitesiEv : HTMLInputElement = document.getElementById('keszitesiEv') as HTMLInputElement;
     let keszitesiEvJo = false;
     if(parseInt(keszitesiEv.value) <= 2022){
         keszitesiEvJo = true;
     }else{
         keszitesiEvJo = false;
+        alert("Nem lehet a készitési év több, mint a jelenlegi évszám");
     }
 
 
-    let kikialtasiAr = document.getElementById('ar') as HTMLInputElement;
+    let kikialtasiAr : HTMLInputElement = document.getElementById('ar') as HTMLInputElement;
     let kikialtasiArJo = false;
     if(parseInt(kikialtasiAr.value) >= 1){
         kikialtasiArJo = true;
     }else{
         keszitesiEvJo =false;
+        alert("Minimum 1Ft kell, hogy legyen")
     }
 
-    let szoborMagassag = document.getElementById('magassag') as HTMLInputElement;
 
+    let szoborMagassag : HTMLInputElement = document.getElementById('magassag') as HTMLInputElement;
+    let szoborMagassagJo = false;
+    if(parseInt(szoborMagassag.value) >= 20){
+        szoborMagassagJo = true;
+    }else{
+        szoborMagassagJo = false;
+        alert("Minimum 20cm magasnak kell lennie a szobornak");
+    }
+    
 
-
+    if(szoborNevVan && keszitesiEvJo && kikialtasiArJo && szoborMagassagJo){
+        szobrok.push(new Statue(szoborNev.value, parseInt(keszitesiEv.value), parseInt(kikialtasiAr.value), parseInt(szoborMagassag.value) ))
+        szoborNev.value = "";
+        keszitesiEv.value = "";
+        kikialtasiAr.value = "";
+        szoborMagassag.value = "";
+    }
 
 
 })
