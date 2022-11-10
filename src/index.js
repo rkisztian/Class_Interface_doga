fe00359decf4f014e78c2355429252df0108ec18
Petrik Diak <diak@petrik.hu>
2022-11-10 14:45:13 +0100

    Nev, Ev és ár validálása talán kész van

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 "use strict";
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
+let szobrok = [];
+(_a = document.getElementById('felvesz')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+    let szoborNev = document.getElementById('szobornev');
+    let szoborNevVan = false;
+    if (szoborNev.value != "") {
+        szoborNevVan = true;
+    }
+    else {
+        szoborNevVan = false;
+    }
+    let keszitesiEv = document.getElementById('keszitesiEv');
+    let keszitesiEvJo = false;
+    if (parseInt(keszitesiEv.value) <= 2022) {
+        keszitesiEvJo = true;
+    }
+    else {
+        keszitesiEvJo = false;
+    }
+    let kikialtasiAr = document.getElementById('ar');
+    let kikialtasiArJo = false;
+    if (parseInt(kikialtasiAr.value) >= 1) {
+        kikialtasiArJo = true;
+    }
+    else {
+        keszitesiEvJo = false;
+    }
+    let szoborMagassag = document.getElementById('magassag');
+});
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,43 @@ export interface Artwork{
     year : number;
     price : number;
 }
+
+
+let szobrok : Artwork[] = [];
+
+document.getElementById('felvesz')?.addEventListener('click', ()=>{
+
+    let szoborNev = document.getElementById('szobornev') as HTMLInputElement;
+    let szoborNevVan = false;
+    if(szoborNev.value != ""){
+        szoborNevVan = true;
+    }else{
+        szoborNevVan = false;
+        
+    }
+
+
+    let keszitesiEv = document.getElementById('keszitesiEv') as HTMLInputElement;
+    let keszitesiEvJo = false;
+    if(parseInt(keszitesiEv.value) <= 2022){
+        keszitesiEvJo = true;
+    }else{
+        keszitesiEvJo = false;
+    }
+
+
+    let kikialtasiAr = document.getElementById('ar') as HTMLInputElement;
+    let kikialtasiArJo = false;
+    if(parseInt(kikialtasiAr.value) >= 1){
+        kikialtasiArJo = true;
+    }else{
+        keszitesiEvJo =false;
+    }
+
+    let szoborMagassag = document.getElementById('magassag') as HTMLInputElement;
+
+
+
+
+
+})
